0a0b65acfa994c567dc16b50b4ea58f91b9effb3
benfzc <benfzcfr.ee@gmail.com>
2024-06-30 08:26:52 +0800

    Update extension to Manifest V3
    - Migrate manifest.json from version 2 to version 3
    - Update background.js to use chrome.action instead of chrome.browserAction
    - Maintain existing functionality while ensuring future compatibility
    - Address deprecation warning for Manifest V2
    
    This commit updates our Chrome extension to use Manifest V3, which is the
    current recommended version by Google. The changes ensure that the extension
    will continue to function beyond 2024 when support for Manifest V2 is scheduled
    to be removed.
    
    Key changes:
    - manifest.json: Updated to version 3 syntax
    - background.js: Modified to use new APIs compatible with Manifest V3

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-chrome.browserAction.onClicked.addListener((tab) => {
+chrome.action.onClicked.addListener((tab) => {
   chrome.storage.sync.get(['readingList'], (result) => {
     let readingList = result.readingList || [];
     readingList.push({ title: tab.title, url: tab.url });
