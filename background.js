0f38594c0d1059072c3f64d80fb0afb467c2c08b
benfzc <benfzcfr.ee@gmail.com>
2024-06-30 08:58:31 +0800

    Fix character encoding and improve internationalization

--- background.js
+++ background.js
@@ -1,9 +1,12 @@
 chrome.action.onClicked.addListener((tab) => {
   chrome.storage.sync.get(['readingList'], (result) => {
     let readingList = result.readingList || [];
-    readingList.push({ title: tab.title, url: tab.url });
+    readingList.push({ 
+      title: tab.title,
+      url: tab.url 
+    });
     chrome.storage.sync.set({ readingList: readingList }, () => {
-      console.log('頁面已加入待讀清單');
+      console.log('Page added to reading list');
     });
   });
 });
